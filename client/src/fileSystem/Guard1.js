283d167d1efe5d41eca8fb0f088c74e19850a7fb
Shannon Peng <pengs@mit.edu>
2020-08-07 15:41:41 -0400

    Add Guard profiles

--- client/src/fileSystem/Guard1.js
+++ client/src/fileSystem/Guard1.js
@@ -1,5 +1,38 @@
 import React, { Component } from 'react';
 
-const Guard1 = <div>Guard1</div>
+const Guard1 =
+	<div className="document-holder">
+        <div className="document-header">
+            <div className="logo">
+                <img src="/government/gov-wordmark-bw.svg" />
+            </div>
+            <div className="guard">
+            	<h6>ID: EI2068</h6>
+            	<img className="guard-portrait" src="/personnel/shakeb.jpg"/>
+            	<h1>Shakeb Asfah</h1>
+
+            	<h3>Biographical Data</h3>
+            	<h6>Sex</h6>
+            	<p>M</p>
+            	<h6>Date of Birth</h6>
+            	<p>1973-02-07</p>
+                <h6>Ethnicity</h6>
+                <p>Asian</p>
+            	<h6>Nationality</h6>
+            	<p>United States</p>
+            	<h6>Employee Since</h6>
+            	<p>2012</p>
+
+            	<h3>Contact Information</h3>
+            	<h6>Home Address</h6>
+            	<p>6267 Carriage Lange, Clifford, PA 18613</p>
+            	<h6>Email Address</h6>
+            	<p>sasfah@dei.us.gov</p>
+            	<h6>Phone Number (Cell)</h6>
+            	<p>+1 (570)-396-8222</p>
+            	
+            </div>
+        </div>
+    </div>
 
 export default Guard1
--- client/src/fileSystem/Guard2.js
+++ client/src/fileSystem/Guard2.js
@@ -1,5 +1,38 @@
 import React, { Component } from 'react';
 
-const Guard2 = <div>Guard2</div>
+const Guard2 =
+	<div className="document-holder">
+        <div className="document-header">
+            <div className="logo">
+                <img src="/government/gov-wordmark-bw.svg" />
+            </div>
+            <div className="guard">
+            	<h6>ID: EI1120</h6>
+            	<img className="guard-portrait" src="/personnel/patricia.jpg"/>
+            	<h1>Patricia Will</h1>
+
+            	<h3>Biographical Data</h3>
+            	<h6>Sex</h6>
+            	<p>F</p>
+            	<h6>Date of Birth</h6>
+            	<p>1972-01-18</p>
+                <h6>Ethnicity</h6>
+                <p>White</p>
+            	<h6>Nationality</h6>
+            	<p>United States</p>
+            	<h6>Employee Since</h6>
+            	<p>2013</p>
+
+            	<h3>Contact Information</h3>
+            	<h6>Home Address</h6>
+            	<p>2881 Charmaine Lane, Meadow, TX 79345</p>
+            	<h6>Email Address</h6>
+            	<p>pwill@dei.us.gov</p>
+            	<h6>Phone Number (Cell)</h6>
+            	<p>+1 (512)-896-0616</p>
+            	
+            </div>
+        </div>
+    </div>
 
 export default Guard2
