5ee9fd3c2a2894506d4161117de3c928a132b74a
Shannon Peng <pengs@mit.edu>
2020-08-13 00:32:14 -0400

    Scroll document viewer and cleanup

--- client/src/components/Document.js
+++ client/src/components/Document.js
@@ -3,16 +3,12 @@ import React, { Component } from 'react';
 import './Document.css';
 
 class Document extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <div className="document-holder">
                 <div className="document-header">
                     <div className="logo">
-                        <img src="/government/gov-wordmark-bw.svg" />
+                        <img src="/government/gov-wordmark-bw.svg" alt="Government logo" />
                     </div>
                 </div>
                 {this.props.children}
