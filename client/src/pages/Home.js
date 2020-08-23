6265c88df8624c2ea480fa1968b6307310f3c575
Shannon Peng <pengs@mit.edu>
2020-08-23 15:40:44 -0400

    Create landing page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Home.css'
 
 
 class Home extends Component {
@@ -11,8 +12,30 @@ class Home extends Component {
 
   render() {
     return (
-    <div className="App">
-      VERS HOME
+    <div className="App home">
+      <div className="content">
+        <img className="logo" src="/logo192.png" />
+        <h1>The VERS Project</h1>
+        <p>Want to help me break into Area 51? Let's get in touch through <a href="//forms.gle/Hhr7qJDJYA21iFAx7">this form</a>.</p>
+        <h3>Core Conspiracists</h3>
+        <ul>
+          <li>@lex</li>
+          <li>Shannon Peng</li>
+          <li>Dillon Zhang</li>
+          <li>Anand Tyagi</li>
+        </ul>
+        <h3>Fact Checkers</h3>
+        <ul>
+          <li>Diana Huang</li>
+          <li>Willy Wu</li>
+          <li>Tiana</li>
+          <li>Justin</li>
+          <li>Pranav</li>
+          <li>Kaitlyn</li>
+        </ul>
+        <h6>With special thanks to <a href="//next.mit.edu/haunt/">MIT Next Haunt</a></h6>
+      </div>
+
     </div>
     );
   }
