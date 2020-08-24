2f3e5db560c0b380c77fed3e6a02bee4cbc48f7a
dillzhang <d1998z@gmail.com>
2020-08-23 21:42:41 -0400

    update directory

--- client/src/fileSystem/Directory.js
+++ client/src/fileSystem/Directory.js
@@ -4,26 +4,72 @@ import Document from "../components/Document";
 const directory = (
     <Document>
         <h2>Floor Directory</h2>
-        <h3>G</h3>
-        <p>
-          Ground Floor Entrance.
-        </p>
-        <h3>S1</h3>
-        <p>
-          Cafeteria and coffee shop.
-        </p>
-        <h3>S2</h3>
-        <p>
-          Meeting rooms A-E.
-        </p>
-        <h3>S3</h3>
-        <p>
-          Merchandise and Arcade room.
-        </p>
-        <h3>S4</h3>
-        <p>
-          Meeting rooms F-L.
-        </p>
+        <h3>Ground Level</h3>
+        <ul>
+          <li>
+            Entrance
+          </li>
+          <li>
+            Abandoned Warehouse Front
+          </li>
+        </ul>
+        <h3>Sublevel 1</h3>
+        <ul>
+          <li>
+            Cafeteria
+          </li>
+          <li>
+            Employee Lockers
+          </li>
+          <li>
+            Showers
+          </li>
+          <li>
+            Breakroom
+          </li>
+        </ul>
+        <h3>Sublevel 2</h3>
+        <ul>
+          <li>
+            Storage
+          </li>
+          <li>
+            Decontamination Room
+          </li>
+        </ul>
+        <h3>Sublevel 3</h3>
+        <ul>
+          <li>
+            Auditorium
+          </li>
+          <li>
+            Library
+          </li>
+          <li>
+            Isaac Room
+          </li>
+          <li>
+            Einstein Room
+          </li>
+          <li>
+            Newton Theater
+          </li>
+          <li>
+            Study
+          </li>
+        </ul>
+        <h3>Sublevel 4</h3>
+        <ul>
+          <li>
+            Lounge
+          </li>
+          <li>
+            Offices
+          </li>
+          <li>
+            Lab
+          </li>
+        </ul>
 
     </Document>
 )
