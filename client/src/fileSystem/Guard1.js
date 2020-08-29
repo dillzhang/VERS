ab903b2f98bb099c37a26c0b09c86775cab1e754
Shannon Peng <pengs@mit.edu>
2020-08-29 13:14:01 -0400

    Update personnel watermarks

--- client/src/fileSystem/Guard1.js
+++ client/src/fileSystem/Guard1.js
@@ -4,47 +4,47 @@ const Guard1 =
 	<div className="document-holder">
         <div className="document-header">
             <div className="logo">
-                <img src="/government/gov-wordmark-bw.svg" />
+                <img src="/government/gov-wordmark-bw.svg" alt="Government logo"/>
             </div>
-            <div className="guard">
-            	<h6>ID: EI2068</h6>
-            	<img className="guard-portrait" src="/personnel/shakeb.jpg" alt="Shakeb Asfah"/>
-            	<h1>Shakeb Asfah</h1>
-                <h4>Resident Linguistician</h4>
+        </div>
+        <div className="guard">
+        	<h6>ID: EI2068</h6>
+        	<img className="guard-portrait" src="/personnel/shakeb.jpg" alt="Shakeb Asfah"/>
+        	<h1>Shakeb Asfah</h1>
+            <h4>Resident Linguistician</h4>
 
-                <h6>Biographical Data</h6>
-                <p><strong>Sex:</strong> M</p>
-                <p><strong>Date of Birth:</strong> 1983-02-07</p>
-                <p><strong>Ethnicity:</strong> Asian</p>
-                <p><strong>Height:</strong> 5' 9"</p>
-                <p><strong>Blood Type:</strong> B+</p>
-                <p><strong>Hair Color:</strong> Black</p>
-                <p><strong>Eye Color:</strong> Brown</p>
-                <p><strong>Education:</strong> B.S. in English Linguistics, Hamdard University</p>
-                <p><strong>Social Security:</strong> 420-37-0068</p>
-                <p><strong>Marital Status:</strong> Married</p>
-                <p><strong>Employee Since:</strong> 2015</p>
+            <h6>Biographical Data</h6>
+            <p><strong>Sex:</strong> M</p>
+            <p><strong>Date of Birth:</strong> 1983-02-07</p>
+            <p><strong>Ethnicity:</strong> Asian</p>
+            <p><strong>Height:</strong> 5' 9"</p>
+            <p><strong>Blood Type:</strong> B+</p>
+            <p><strong>Hair Color:</strong> Black</p>
+            <p><strong>Eye Color:</strong> Brown</p>
+            <p><strong>Education:</strong> B.S. in English Linguistics, Hamdard University</p>
+            <p><strong>Social Security:</strong> 420-37-0068</p>
+            <p><strong>Marital Status:</strong> Married</p>
+            <p><strong>Employee Since:</strong> 2015</p>
 
-                <h6>Contact Information</h6>
-                <p><strong>Home Address:</strong><br/>6267 Carriage Lange<br/> Clifford, PA 18613</p>
-                <p><strong>Email Address:</strong><br/>sasfah4283@dei.us.gov</p>
-                <p><strong>Phone Number (Cell):</strong><br/>+1 (555)-396-8222</p>
+            <h6>Contact Information</h6>
+            <p><strong>Home Address:</strong><br/>6267 Carriage Lange<br/> Clifford, PA 18613</p>
+            <p><strong>Email Address:</strong><br/>sasfah4283@dei.us.gov</p>
+            <p><strong>Phone Number (Cell):</strong><br/>+1 (555)-396-8222</p>
 
-                <h6>Employment Details</h6>
-                <p><strong>Title:</strong> Resident Linguistician</p>
-                <p><strong>Division:</strong> LNG - Linguistics Research</p>
-                <p><strong>Location:</strong> Office 3</p>
-                <p><strong>Supervisor:</strong> Sheena Ward, Head of Linguistics Research</p>
-                <p><strong>Monthly Salary:</strong> $4,300</p>
+            <h6>Employment Details</h6>
+            <p><strong>Title:</strong> Resident Linguistician</p>
+            <p><strong>Division:</strong> LNG - Linguistics Research</p>
+            <p><strong>Location:</strong> Office 3</p>
+            <p><strong>Supervisor:</strong> Sheena Ward, Head of Linguistics Research</p>
+            <p><strong>Monthly Salary:</strong> $4,300</p>
 
-                <h6>Spotlight Interview</h6>
-                <p><strong>How did you choose your field of study?</strong></p>
-                <p>I have been fascinated by languages ever since I was young. By the time I attended college, I knew I wanted to attend a linguistics program.</p>
-                <p><strong>What fascinates you about languages?</strong></p>
-                <p>How integral they are to culture. By learning a new language, you not only understand what its native speakers are saying, you also open your eyes to a whole new way of life, a different way of experiencing the world.</p>
-                <p><strong>What brought you to the Department of Extraterrestrial Investigation?</strong></p>
-                <p>The chance to examine alien languages before the rest of the world sees them. My life will be complete once I can learn an extraterrestrial language. Who knows what the cultures are like out in space?</p>
-                </div>
+            <h6>Spotlight Interview</h6>
+            <p><strong>How did you choose your field of study?</strong></p>
+            <p>I have been fascinated by languages ever since I was young. By the time I attended college, I knew I wanted to attend a linguistics program.</p>
+            <p><strong>What fascinates you about languages?</strong></p>
+            <p>How integral they are to culture. By learning a new language, you not only understand what its native speakers are saying, you also open your eyes to a whole new way of life, a different way of experiencing the world.</p>
+            <p><strong>What brought you to the Department of Extraterrestrial Investigation?</strong></p>
+            <p>The chance to examine alien languages before the rest of the world sees them. My life will be complete once I can learn an extraterrestrial language. Who knows what the cultures are like out in space?</p>
         </div>
     </div>
 
