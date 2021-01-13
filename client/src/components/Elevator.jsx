a8056a97925b1f5dad3fe2b7591d7ee691d2f5c4
Shannon Peng <pengs@mit.edu>
2021-01-12 23:30:44 -0500

    feat(elevator): change elevator sound by floor delta

--- client/src/components/Elevator.jsx
+++ client/src/components/Elevator.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import "./Elevator.css";
 
-const ELEVATOR_DURATION = 12000;
+const ELEVATOR_DURATION = 10000;
 const FLOORS = ["G", "S1", "S2", "S3", "S4"];
 
 function Elevator(props) {
@@ -11,13 +11,23 @@ function Elevator(props) {
   const [isDisabled, setDisabled] = useState(false);
   const [elevatorSounds] = useState(["S2_elevator_1_floor_delta", "S2_elevator_2_floor_delta", "S2_elevator_3_floor_delta", "S2_elevator_4_floor_delta"]);
   const [floorList] = useState(["G", "S1", "S2", "S3", "S4"]);
+  const [soundDurations] = useState([12000, 18000, 27000, 30000]);
+  const [d_i, setDI] = useState(0);
 
-  const load_floor = (floor) => {
-    props.globalPlaySound("S2_elevator_1_floor_delta");
+  const get_floor_delta_index = (f1, f2) => {
+    return parseInt(Math.abs(floorList.indexOf(f1) - floorList.indexOf(f2))) - 1;
+  }
+
+  const load_floor = (f) => {
+    var d = get_floor_delta_index(floor, f)
+    setDI(d);
+    console.log(d, d_i);
+    props.globalPlaySound(elevatorSounds[d]);
+    // setTimeout(() => { props.globalStopSound(elevatorSounds[d_i]) }, 1000);
     setTimeout(() => {
       setDisabled(false); // count is 0 here
-      setFloor(floor);
-      switch (floor) {
+      setFloor(f);
+      switch (f) {
         case "G":
           setText(
             "WRONG: Looks like this is where we came in from. Let's check out the other floors."
@@ -47,10 +57,10 @@ function Elevator(props) {
           setText("Something wrong has occurred");
           break;
       }
-    }, ELEVATOR_DURATION);
+    }, soundDurations[d]);
     setFloor("-");
     setDisabled(true);
-    setText(`Going to Floor ${floor}...`);
+    setText(`Going to Floor ${f}...`);
   };
 
   useEffect(() => {}, []);
@@ -79,7 +89,7 @@ function Elevator(props) {
       <div className="status-container">
         <p>Status:</p>
         <div className={`progress-bar ${isDisabled ? "active" : "inactive"}`}>
-          <span className={`meter`} />
+          <span style={ isDisabled ? {"transition": `width ${soundDurations[d_i]}ms linear` } : {} }className={`meter`} />
         </div>
       </div>
       <p className="status-output">{text}</p>
