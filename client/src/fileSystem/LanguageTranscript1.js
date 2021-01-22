470b03405506fb0cf6b04ed98d984a55f5d79c4b
Shannon Peng <pengs@mit.edu>
2021-01-22 17:51:14 -0500

    fix(translator): fix font case inconsistencies for translator

--- client/src/fileSystem/LanguageTranscript1.js
+++ client/src/fileSystem/LanguageTranscript1.js
@@ -8,39 +8,39 @@ const languageTranscript1 =
             <h6>INTERVIEW TRANSCRIPT</h6>
             <h6>SUBJECT ID #7104</h6>
             <p><strong>SUBJECT:</strong> Where am I?</p>
-            <p><strong>INTERVIEWER 1:</strong> Ƕ∂ỼϬ∫. Quiet!</p>
-            <p><strong>INTERVIEWER 2:</strong> ⑃Վ §Վ∂ ᲯՄՎ‰ ‰చට∫ ⱮచϬ'Ɱ Ɱට§ỼՄԱ?</p>
-            <p><strong>I2:</strong> ⱮచϬ'Ɱ ටⱮᲯỼՄԱ ‰చϬلϬ ⱮచϬ ỼⱮ.</p>
+            <p><strong>INTERVIEWER 1:</strong> <span className="alien">Ƕ∂ỼϬ∫</span>. Quiet!</p>
+            <p><strong>INTERVIEWER 2:</strong> <span className="alien">⑃Վ §Վ∂ ᲯՄՎ‰ ‰చට∫ ⱮచϬ'Ɱ Ɱට§ỼՄԱ?</span></p>
+            <p><strong>I2:</strong> <span className="alien">ⱮచϬ'Ɱ ටⱮᲯỼՄԱ ‰చϬلϬ ⱮచϬ ỼⱮ.</span></p>
             <p><strong>I2:</strong> You are in a government facility. Do not worry, you are safe.</p>
             <p><strong>S:</strong> What year is it? Where is my family?</p>
-            <p><strong>I1:</strong> ‰చට∫ ỼⱮ ⱮచϬ Ɱට§ỼՄԱ?</p>
-            <p><strong>I2:</strong> ⱮచϬ ỼⱮ ටⱮᲯỼՄԱ ҨՎل ∫చϬ §Ϭටل ටՄ⑃ చϬل ҨටՀỼហ§.</p>
+            <p><strong>I1:</strong> <span className="alien">‰చට∫ ỼⱮ ⱮచϬ Ɱට§ỼՄԱ?</span></p>
+            <p><strong>I2:</strong> <span className="alien">ⱮచϬ ỼⱮ ටⱮᲯỼՄԱ ҨՎل ∫చϬ §Ϭටل ටՄ⑃ చϬل ҨටՀỼហ§.</span></p>
             <p><strong>I2:</strong> The year is 2016. Your husband and kids are safe in their home.</p>
             <p><strong>S:</strong> Home? They're alive?! Why am I here?</p>
             <p><strong>S:</strong> What language are you speaking?</p>
             <p><strong>S:</strong> Who are you? Tell me!</p>
-            <p><strong>I1:</strong> Ƕ∂ỼϬ∫!</p>
+            <p><strong>I1:</strong> <span className="alien">Ƕ∂ỼϬ∫!</span></p>
             <p><strong>S:</strong> ...</p>
             <p><strong>I2:</strong> We have many questions to ask you.</p>
             <p><strong>I2:</strong> Can you tell us what is this?</p>
             <p><strong>S:</strong> A photograph?</p>
-            <p><strong>I2:</strong> ට ↻చՎ∫ՎԱلට↻చ.</p>
-            <p><strong>I1:</strong> ՎᲯ.</p>
+            <p><strong>I2:</strong> <span className="alien">ට ↻చՎ∫ՎԱلට↻చ.</span></p>
+            <p><strong>I1:</strong> <span className="alien">ՎᲯ.</span></p>
             <p><strong>I2:</strong> What is a photograph?</p>
             <p><strong>S:</strong> Are you serious?</p>
             <p><strong>S:</strong> It's a picture. To capture a memory?</p>
             <p><strong>S:</strong> How do you not know what a photo is?</p>
-            <p><strong>I2:</strong> ට ՀϬՀՎل§.</p>
+            <p><strong>I2:</strong> <span className="alien">ට ՀϬՀՎل§.</span></p>
             <p><strong>I2:</strong> What is a memory?</p>
             <p><strong>S:</strong> It's something that happened in the past.</p>
-            <p><strong>I2:</strong> ∫చϬ ↻ටⱮ∫.</p>
-            <p><strong>I1:</strong> ՎᲯ.</p>
+            <p><strong>I2:</strong> <span className="alien">∫చϬ ↻ටⱮ∫.</span></p>
+            <p><strong>I1:</strong> <span className="alien">ՎᲯ.</span></p>
             <p><strong>I2:</strong> What is this past you speak of?</p>
             <p><strong>S:</strong> The past is something that's gone. Like my patience.</p>
-            <p><strong>I1:</strong> Ɱ∫ට§ ⑃Վ‰Մ! Stay down!</p>
+            <p><strong>I1:</strong> <span className="alien">Ɱ∫ට§ ⑃Վ‰Մ!</span> Stay down!</p>
             <p><strong>S:</strong> I'm sick of this. Who are you? Tell me now!</p>
-            <p><strong>I2:</strong> ∫∂لՄ ՎҨҨ ∫చϬ ՀỼ९لՎ↻చՎՄϬ.</p>
-            <p><strong>I1:</strong> ՎᲯ.</p>
+            <p><strong>I2:</strong> <span className="alien">∫∂لՄ ՎҨҨ ∫చϬ ՀỼ९لՎ↻చՎՄϬ.</span></p>
+            <p><strong>I1:</strong> <span className="alien">ՎᲯ.</span></p>
         </div>
       </div>
 
--- client/src/fileSystem/LanguageTranscript2.js
+++ client/src/fileSystem/LanguageTranscript2.js
@@ -11,24 +11,24 @@ const languageTranscript2 =
         	<p><strong>SUBJECT:</strong> What?</p>
         	<p><strong>I2:</strong> What is the past?</p>
         	<p><strong>S:</strong> The past is what's behind us in time. What we leave behind.</p>
-        	<p><strong>I2:</strong> ∫చϬ ↻ටⱮ∫ ỼⱮ ‰చට∫'Ɱ ⅋ϬచỼՄ⑃ ∂Ɱ ỼՄ ∫ỼՀϬ. ‰చට∫ ‰Ϭ ហϬට◊Ϭ ⅋ϬచỼՄ⑃.</p>
-        	<p><strong>INTERVIEWER 1:</strong> ՎᲯ.</p>
+        	<p><strong>I2:</strong> <span className="alien">∫చϬ ↻ටⱮ∫ ỼⱮ ‰చට∫'Ɱ ⅋ϬచỼՄ⑃ ∂Ɱ ỼՄ ∫ỼՀϬ. ‰చට∫ ‰Ϭ ហϬට◊Ϭ ⅋ϬచỼՄ⑃.</span></p>
+        	<p><strong>INTERVIEWER 1:</strong> <span className="alien">ՎᲯ.</span></p>
         	<p><strong>I2:</strong> How do you know what is in the past?</p>
         	<p><strong>S:</strong> You have a memory of it.</p>
-        	<p><strong>I2:</strong> §Վ∂ చට◊Ϭ ට ՀϬՀՎل§ ՎҨ Ỽ∫.</p>
-        	<p><strong>I1:</strong> ՎᲯ.</p>
+        	<p><strong>I2:</strong> <span className="alien">§Վ∂ చට◊Ϭ ට ՀϬՀՎل§ ՎҨ Ỽ∫.</span></p>
+        	<p><strong>I1:</strong> <span className="alien">ՎᲯ.</span></p>
         	<p><strong>I2:</strong> Where do you keep this memory?</p>
         	<p><strong>S:</strong> You just keep it in your brain?</p>
-        	<p><strong>I2:</strong> §Վ∂ Ӌ∂Ɱ∫ ᲯϬϬ↻ Ỽ∫ ỼՄ §Վ∂ل ⅋لටỼՄ.</p>
-        	<p><strong>I1:</strong> ՎᲯ.</p>
+        	<p><strong>I2:</strong> <span className="alien">§Վ∂ Ӌ∂Ɱ∫ ᲯϬϬ↻ Ỽ∫ ỼՄ §Վ∂ل ⅋لටỼՄ.</span></p>
+        	<p><strong>I1:</strong> <span className="alien">ՎᲯ.</span></p>
 			<p><strong>I2:</strong> How?</p>
 			<p><strong>S:</strong> Well... your brain is made up of cells called neurons? And memory is stored in neuron firing patterns.</p>
 			<p><strong>S:</strong> I mean, you must remember from biology class, right? Is this some sort of test?</p>
-			<p><strong>I2:</strong> §Վ∂ل ⅋لටỼՄ ỼⱮ Հට⑃Ϭ ∂↻ ՎҨ ९ϬហហⱮ ९ටហហϬ⑃ ՄϬ∂لՎՄⱮ, ටՄ⑃ ՀϬՀՎل§ ỼⱮ Ɱ∫ՎلϬ⑃ ỼՄ ՄϬ∂لՎՄ ҨỼلỼՄԱ ↻ට∫∫ϬلՄⱮ.</p>
-        	<p><strong>I1:</strong> ՎᲯ.</p>
+			<p><strong>I2:</strong> <span className="alien">§Վ∂ل ⅋لටỼՄ ỼⱮ Հට⑃Ϭ ∂↻ ՎҨ ९ϬហហⱮ ९ටហហϬ⑃ ՄϬ∂لՎՄⱮ, ටՄ⑃ ՀϬՀՎل§ ỼⱮ Ɱ∫ՎلϬ⑃ ỼՄ ՄϬ∂لՎՄ ҨỼلỼՄԱ ↻ට∫∫ϬلՄⱮ.</span></p>
+        	<p><strong>I1:</strong> <span className="alien">ՎᲯ.</span></p>
 			<p><strong>I2:</strong> Thank you for your answers.</p>
-			<p><strong>I2:</strong> ҨហටⱮచ చỼՀ.</p>
-        	<p><strong>I1:</strong> ՎᲯ.</p>
+			<p><strong>I2:</strong> <span className="alien">ҨហටⱮచ చỼՀ.</span></p>
+        	<p><strong>I1:</strong> <span className="alien">ՎᲯ.</span></p>
         </div>
     </div>
 
